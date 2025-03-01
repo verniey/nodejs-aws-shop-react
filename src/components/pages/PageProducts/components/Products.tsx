b3c260d85394cf79fd6dd23334de54c7a9d76e06
Veronika <veronika@Veronikas-MacBook-Pro.local>
2025-03-01 12:06:52 +0100

    update Products

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -18,24 +18,24 @@ export default function Products() {
   return (
     <Grid container spacing={4}>
       {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
-      {data.map(({ count, ...product }, index) => (
+      {data.map(({count, ...product}, index) => (
         <Grid item key={product.id} xs={12} sm={6} md={4}>
           <Card
-            sx={{ height: "100%", display: "flex", flexDirection: "column" }}
+            sx={{height: "100%", display: "flex", flexDirection: "column"}}
           >
             <CardMedia
-              sx={{ pt: "56.25%" }}
-              image={`https://source.unsplash.com/random?sig=${index}`}
+              sx={{pt: "56.25%"}}
+              image={`https://picsum.photos/400/225?random=${index}`}
               title="Image title"
             />
-            <CardContent sx={{ flexGrow: 1 }}>
+            <CardContent sx={{flexGrow: 1}}>
               <Typography gutterBottom variant="h5" component="h2">
                 {product.title}
               </Typography>
               <Typography>{formatAsPrice(product.price)}</Typography>
             </CardContent>
             <CardActions>
-              <AddProductToCart product={product} />
+              <AddProductToCart product={product}/>
             </CardActions>
           </Card>
         </Grid>
