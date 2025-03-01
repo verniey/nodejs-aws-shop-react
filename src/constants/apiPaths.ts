407638b3d4c23fb23293ada6f5b242bc8fa98508
Veronika <veronika@Veronikas-MacBook-Pro.local>
2025-03-01 12:05:46 +0100

    update API_PATHS with actual URL

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://lsaaozqyr5.execute-api.eu-west-1.amazonaws.com/prod",
+  product: "https://8c0xz8gwof.execute-api.eu-west-1.amazonaws.com/prod",
   order: "https://lsaaozqyr5.execute-api.eu-west-1.amazonaws.com/prod",
   import: "https://lsaaozqyr5.execute-api.eu-west-1.amazonaws.com/prod",
-  bff: "https://lsaaozqyr5.execute-api.eu-west-1.amazonaws.com/prod",
+  bff: "https://8c0xz8gwof.execute-api.eu-west-1.amazonaws.com/prod",
   cart: "https://lsaaozqyr5.execute-api.eu-west-1.amazonaws.com/prod",
 };
 
